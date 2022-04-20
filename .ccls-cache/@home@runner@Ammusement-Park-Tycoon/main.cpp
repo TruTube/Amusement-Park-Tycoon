1a96ae77dc1541096ee158cd7fd8b6d0300ffcc2
Kanjo <37850129+TruTube@users.noreply.github.com>
2022-04-20 00:04:54 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Ammusement-Park-Tycoon/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//Variables:
+int userinputnum;
+char userinputchar;
+int numofattractions;
+int numofemployees;
+int numofplots;
+float visitorincrease;
+int numofvisitors;
+std::vector<std::string> attractions(1);
+
+class ammusementpark {
+  void addattractions() {
+    std::cout << "What attraction would you like to build?\n 1. Roller Coaster \n 2. Short Movie Experience \n 3. Tower Drop";
+    std::cin >> userinputnum;
+  }
+  void hireemployees(int numofhires) {
+    
+  }
+};
+
+int main() {
+
+}
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class amusement_park {
+
+public: 
+
+//Variables:
+
+//Functions:
+
+
+};
+
+//Random function:
+//   // int random(int lo, int hi) {
+//   //   thread_local std::random_device rd;
+//   //   thread_local std::mt19937 gen(rd());
+//   //   std::uniform_int_distribution<int> dis(lo, hi);
+//   //   return dis(gen); //don't know how this works, someone gave it to me
+// }
+
+int main(){
+  
+}
